bbb94a39098746f978b39debdd466be3caff78c7
JazminAime <jazminfalcon@hotmail.com>
2025-03-28 12:59:51 -0300

    seccion videos

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,42 @@
+import { Box, Text } from "@chakra-ui/react";
+
+const VideoCard = ({ video }) => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="12px"
+      p="4"
+      w={{ base: "100%", md: "650px" }}
+      h={{ base: "100%", md: "450px" }}
+      textAlign="center"
+      bg="rgba(255, 255, 255, 0.1)"
+      backdropFilter="blur(10px)"
+      border="2px solid rgba(255, 255, 255, 0.3)"
+      boxShadow="0px 0px 10px rgba(255, 255, 255, 0.2)"
+      transition="all 0.3s ease-in-out"
+      _hover={{
+        transform: "translateY(-5px)",
+        boxShadow: "0px 0px 20px rgba(255, 255, 255, 0.5)",
+      }}>
+      <Text color="custom.50" fontWeight="bold" mb="3" fontSize="xl" mt="5px">
+        {video.title}
+      </Text>
+      <iframe
+        width="100%"
+        height="400px"
+        src={`https://www.youtube.com/embed/${video.youtubeId}`}
+        title={video.title}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        style={{
+          borderRadius: "8px",
+          marginBottom: "10px",
+        }}></iframe>
+    </Box>
+  );
+};
+
+export default VideoCard;
--- src/pages/VideoList.jsx
+++ src/pages/VideoList.jsx
@@ -1,5 +1,48 @@
+import { Flex, Heading, SimpleGrid } from "@chakra-ui/react";
+import VideoCard from "../components/VideoCard";
+
 const VideoList = () => {
-  return <div></div>;
+  const videos = [
+    {
+      title: "Seminare (cover versionado de Serú Girán)",
+      youtubeId: "Gqx30K_h-ag",
+    },
+    {
+      title: "La pachanga (cover versionado de Vilma Palma e Vampiros)",
+      youtubeId: "jNq5--u_3gs",
+    },
+    {
+      title: "It's my Life - acústico (cover versionado de Bon Jovi)",
+      youtubeId: "t5fU6ijckjQ",
+    },
+    {
+      title: "Cosas mías (cover versionado de Los Abuelos de la Nada)",
+      youtubeId: "4HeFenvGufA",
+    },
+    {
+      title: "Mi viejo corazón (autoría propia)",
+      youtubeId: "5Gb2kvXhPFA",
+    },
+  ];
+  return (
+    <>
+      <Heading
+        marginTop="120px"
+        textAlign="center"
+        color="custom.50"
+        textShadow="0 0 10px #b71c1c, 0 0 20px #7f0000, 0 0 30px #ff1744"
+        mb="8">
+        Videos
+      </Heading>
+      <Flex justify="center">
+        <SimpleGrid columns={1} spacing="8" px="4" maxW="1200px" mb="50px">
+          {videos.map((video, index) => (
+            <VideoCard key={index} video={video} />
+          ))}
+        </SimpleGrid>
+      </Flex>
+    </>
+  );
 };
 
 export default VideoList;
